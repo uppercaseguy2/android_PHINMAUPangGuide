df1eb52efc360f67b189b248a420aab28102b622
uppercaseguy2 <marshmallenn@gmail.com>
2021-11-09 13:13:17 +0800

    adds selection menu

--- app/src/main/java/com/teamuppercaseguy2/android_PHINMAUPangGuide/HelpSelectActivity.java
+++ app/src/main/java/com/teamuppercaseguy2/android_PHINMAUPangGuide/HelpSelectActivity.java
@@ -10,5 +10,6 @@ public class HelpSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_select);
+
     }
 }
